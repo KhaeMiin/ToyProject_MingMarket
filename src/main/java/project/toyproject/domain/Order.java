8244b31555d9a652ed5e0d6f7c4eb6afba006cb3
minMin <goals1005@naver.com>
2022-05-10 23:12:12 +0900

    도메인 수정

--- src/main/java/project/toyproject/domain/Order.java
+++ src/main/java/project/toyproject/domain/Order.java
@@ -1,11 +1,21 @@
 package project.toyproject.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Order {
 
+    @Id @GeneratedValue
     private Long orderId;
-    private Member member; //FK
+
+    @ManyToOne
+    private Member member; //FK (현재 클래스가 연관관계 주인) : 회원 한명은 여러개의 주문을 할 수 있다.
     private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    public Order() {
+    }
 }
